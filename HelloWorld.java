64a84b2e6f23b36793ac107d1a5b044b2e9387bd
NihiKarthick <145859079+NihiKarthick@users.noreply.github.com>
2023-10-12 16:23:33 +0530

    Initial Commit

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+ public static void main (String[] args){
+ // TODO Auto-generated method stub
+ 
+ System.out.println("Hello Karthick");
+ System.out.println("Hello Germany");
+ 
+ }
+ }
+
+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--Git Hub Token1
+ 
